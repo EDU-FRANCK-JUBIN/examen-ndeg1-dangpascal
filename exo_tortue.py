917156ffbec4c8dc4de447446d03a75442172281
Pascal Dang <pascaldang@MacBook-Pro-de-Pascal.local>
2019-12-20 12:22:22 +0100

    Correction turtle

---
+++ exo_tortue.py
@@ -0,0 +1,8 @@
+import turtle as tu
+import random
+
+window = tu.getscreen()
+window.clear()
+window.bgpic("champcourse2.gif")
+window.setup(width=1920, height=1080, startx=0, starty=0)
+
--- turtle.py
+++
@@ -1,8 +0,0 @@
-import turtle
-import random
-
-windows = turtle.getscreen()
-windows.clear()
-windows.bgpic("champcourse2.gif")
-windows.setup(width=1920, height=1080, startx=0, starty=0)
-
